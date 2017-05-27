53acc239c324ec27a945a337ada43b80bc636f89
Vital <vpetruk.osn@gmail.com>
2017-05-28 00:51:02 +0300

    Added scripts for movement and mouse look. Both need to use in the same time.

---
+++ Scripts/MoveCharacter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour {
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate(Vector3.forward * Time.deltaTime, Space.Self);
+		
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate(Vector3.back * Time.deltaTime, Space.Self);
+
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate(Vector3.left * Time.deltaTime, Space.Self);
+
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate(Vector3.right * Time.deltaTime, Space.Self);
+
+		}
+	}
+	void FixedUpdate(){
+		
+
+	}
+}
