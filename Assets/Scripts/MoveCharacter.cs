6db049d063ebb6498d07f880148de7ec28a70b5b
ValeriaMakarenko <makarenkovaleria3@gmail.com>
2017-05-28 23:39:00 +0300

    update-mix-28,

---
+++ Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour {
+
+	private Animator anim;
+
+	void Start() {
+		anim = GetComponent<Animator>();
+	}
+
+	void Update () {
+
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.D)) {
+			anim.Play("Walk");
+		} else {
+			anim.Play("Idle");
+		}
+
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate(Vector3.forward * Time.deltaTime*10, Space.Self);
+		
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate(Vector3.back * Time.deltaTime*10, Space.Self);
+
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate(Vector3.left * Time.deltaTime*10, Space.Self);
+
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate(Vector3.right * Time.deltaTime*10, Space.Self);
+
+		}
+		
+	}
+
+	void FixedUpdate(){
+	}
+}
